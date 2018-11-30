cfd0e16333888fcb384864e9a2d6f5a2565eb441
bbb <bbbb@163.com>
2018-11-30 14:40:48 +0800

    optimize homescreen time

--- WebDriverAgentLib/Categories/XCUIDevice+FBHelpers.m
+++ WebDriverAgentLib/Categories/XCUIDevice+FBHelpers.m
@@ -23,7 +23,7 @@
 #import "XCAXClient_iOS.h"
 #import "XCUIScreen.h"
 
-static const NSTimeInterval FBHomeButtonCoolOffTime = 1.;
+//static const NSTimeInterval FBHomeButtonCoolOffTime = 1.;
 
 @implementation XCUIDevice (FBHelpers)
 
@@ -35,10 +35,12 @@ static const NSTimeInterval FBHomeButtonCoolOffTime = 1.;
   // So if we don't wait here it will be interpreted as double home button gesture and go to application switcher instead.
   // On 9.3 pressButton:XCUIDeviceButtonHome can be slightly delayed.
   // Causing waitUntilApplicationBoardIsVisible not to work properly in some edge cases e.g. like starting session right after this call, while being on home screen
+  /*
   [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:FBHomeButtonCoolOffTime]];
   if (![[FBSpringboardApplication fb_springboard] fb_waitUntilApplicationBoardIsVisible:error]) {
     return NO;
   }
+  */
   return YES;
 }
 
